b8b8029c7368ac0a66cfb750801cf0a1c923369b
RESHMAKOTIPALLI <reshmakotipalli793@gmail.com>
2024-09-18 16:47:10 +0530

    Update product1.jsx

--- src/Data/product1.jsx
+++ src/Data/product1.jsx
@@ -5,6 +5,23 @@ import camera3 from '../Assets/camera3.jpg'
 import camera4 from '../Assets/camera4.jpg'
 import camera5 from '../Assets/camera5.jpg'
 import camera6 from '../Assets/camera6.jpg'
+import camera7 from '../Assets/camera7.jpg'
+import camera8 from '../Assets/camera8.jpg'
+import camera9 from '../Assets/camera9.jpg'
+import camera10 from '../Assets/camera10.jpg'
+import camera11 from '../Assets/camera11.jpg'
+import camera12 from '../Assets/camera12.jpg'
+import camera13 from '../Assets/camera13.jpg'
+import camera14 from '../Assets/camera14.jpg'
+import camera15 from '../Assets/camera15.jpg'
+import camera16 from '../Assets/camera16.jpg'
+import camera17 from '../Assets/camera17.jpg'
+import camera18 from '../Assets/camera18.jpg'
+import camera19 from '../Assets/camera19.jpg'
+import camera20 from '../Assets/camera20.jpg'
+
+
+
 
 
 export const productsData = [
@@ -43,7 +60,102 @@ export const productsData = [
       "ProductName":"leica",
       "Price":"16000",
 
-  }, 
+  },
+  {
+    "image":camera7,
+    "ProductName":"leica",
+    "Price":"16000",
 
- 
+}, 
+
+{
+    "image":camera8,
+    "ProductName":"leica",
+    "Price":"16000",
+
+}, 
+
+{
+    "image":camera9,
+    "ProductName":"leica",
+    "Price":"16000",
+
+}, 
+
+{
+    "image":camera10,
+    "ProductName":"leica",
+    "Price":"16000",
+
+}, 
+
+{
+    "image":camera11,
+    "ProductName":"leica",
+    "Price":"16000",
+
+}, 
+
+{
+    "image":camera12,
+    "ProductName":"leica",
+    "Price":"16000",
+
+}, 
+
+{
+    "image":camera13,
+    "ProductName":"leica",
+    "Price":"16000",
+
+}, 
+
+{
+    "image":camera14,
+    "ProductName":"leica",
+    "Price":"16000",
+
+}, 
+
+{
+    "image":camera15,
+    "ProductName":"leica",
+    "Price":"16000",
+
+}, 
+
+{
+    "image":camera16,
+    "ProductName":"leica",
+    "Price":"16000",
+
+}, 
+
+{
+    "image":camera17,
+    "ProductName":"leica",
+    "Price":"16000",
+
+}, 
+
+{
+    "image":camera18,
+    "ProductName":"leica",
+    "Price":"16000",
+
+}, 
+
+{
+    "image":camera19,
+    "ProductName":"leica",
+    "Price":"16000",
+
+}, 
+
+{
+    "image":camera20,
+    "ProductName":"leica",
+    "Price":"16000",
+
+}, 
 ]

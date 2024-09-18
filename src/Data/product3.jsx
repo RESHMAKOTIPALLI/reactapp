33b15263c8bf8c4a1addd3010137004db0274dca
RESHMAKOTIPALLI <reshmakotipalli793@gmail.com>
2024-09-18 16:48:08 +0530

    Update product3.jsx

--- src/Data/product3.jsx
+++ src/Data/product3.jsx
@@ -1,9 +1,148 @@
 import React from 'react'
+import headphone1 from '../Assets/headphone1.jpg'
+import headphone2 from '../Assets/headphone2.jpg'
+import headphone3 from '../Assets/headphone3.jpg'
+import headphone4 from '../Assets/headphone4.jpg'
+import headphone5 from '../Assets/headphone5.jpg'
+import headphone6 from '../Assets/headphone6.jpg'
+import headphone7 from '../Assets/headphone7.jpg'
+import headphone8 from '../Assets/headphone8.jpg'
+import headphone9 from '../Assets/headphone9.jpg'
+import headphone10 from '../Assets/headphone10.jpg'
+import headphone11 from '../Assets/headphone11.jpg'
+import headphone12 from '../Assets/headphone12.jpg'
+import headphone13 from '../Assets/headphone13.jpg'
+import headphone14 from '../Assets/headphone14.jpg'
+import headphone15 from '../Assets/headphone15.jpg'
+import headphone16 from '../Assets/headphone16.jpg'
+import headphone17 from '../Assets/headphone17.jpg'
+import headphone18 from '../Assets/headphone18.jpg'
+import headphone19 from '../Assets/headphone19.jpg'
+import headphone20 from '../Assets/headphone20.jpg'
 
-function product() {
-  return (
-    <div>product</div>
-  )
-}
 
-export default product
+
+export const productsData = [
+  {
+      "image":headphone1,
+      "ProductName":"",
+      "Price":"2500",
+
+  },
+  {
+      "image":headphone2,
+      "ProductName":"",
+      "Price":"3800",
+
+  },
+  {
+      "image":headphone3,
+      "ProductName":"",
+      "Price":"2000",
+
+  },
+  {
+      "image":headphone4,
+      "ProductName":"",
+      "Price":"5200",
+
+  }, 
+  {
+      "image":headphone5,
+      "ProductName":"",
+      "Price":"4800",
+
+  },
+  {
+      "image":headphone6,
+      "ProductName":"",
+      "Price":"5000",
+
+  }, 
+  {
+    "image":headphone7,
+    "ProductName":"",
+    "Price":"5000",
+
+}, 
+{
+    "image":headphone8,
+    "ProductName":"",
+    "Price":"5000",
+
+}, 
+{
+    "image":headphone9,
+    "ProductName":"",
+    "Price":"5000",
+
+}, 
+{
+    "image":headphone10,
+    "ProductName":"",
+    "Price":"5000",
+
+}, 
+{
+    "image":headphone11,
+    "ProductName":"",
+    "Price":"5000",
+
+}, 
+{
+    "image":headphone12,
+    "ProductName":"",
+    "Price":"5000",
+
+}, 
+{
+    "image":headphone13,
+    "ProductName":"",
+    "Price":"5000",
+
+}, 
+{
+    "image":headphone14,
+    "ProductName":"",
+    "Price":"5000",
+
+}, 
+{
+    "image":headphone15,
+    "ProductName":"",
+    "Price":"5000",
+
+}, 
+{
+    "image":headphone16,
+    "ProductName":"",
+    "Price":"5000",
+
+}, 
+{
+    "image":headphone17,
+    "ProductName":"",
+    "Price":"5000",
+
+}, 
+{
+    "image":headphone18,
+    "ProductName":"",
+    "Price":"5000",
+
+}, 
+{
+    "image":headphone19,
+    "ProductName":"",
+    "Price":"5000",
+
+}, 
+{
+    "image":headphone20,
+    "ProductName":"",
+    "Price":"5000",
+
+}, 
+
+ 
+]

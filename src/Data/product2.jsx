2468247c0719d0483b216e072a8acd367febfa1b
RESHMAKOTIPALLI <reshmakotipalli793@gmail.com>
2024-09-18 16:47:33 +0530

    Update product2.jsx

--- src/Data/product2.jsx
+++ src/Data/product2.jsx
@@ -1,49 +1,147 @@
 import React from 'react'
 import laptop1 from '../Assets/laptop1.jpg'
-import laptop2 from '../Assets/camera2.jpg'
-import camera3 from '../Assets/camera3.jpg'
-import camera4 from '../Assets/camera4.jpg'
-import camera5 from '../Assets/camera5.jpg'
-import camera6 from '../Assets/camera6.jpg'
+import laptop2 from '../Assets/laptop2.jpg'
+import laptop3 from '../Assets/laptop3.jpg'
+import laptop4 from '../Assets/laptop4.jpg'
+import laptop5 from '../Assets/laptop5.jpg'
+import laptop6 from '../Assets/laptop6.jpg'
+import laptop7 from '../Assets/laptop7.jpg'
+import laptop8 from '../Assets/laptop8.jpg'
+import laptop9 from '../Assets/laptop9.jpg'
+import laptop10 from '../Assets/laptop10.jpg'
+import laptop11 from '../Assets/laptop11.jpg'
+import laptop12 from '../Assets/laptop12.jpg'
+import laptop13 from '../Assets/laptop13.jpg'
+import laptop14 from '../Assets/laptop14.jpg'
+import laptop15 from '../Assets/laptop15.jpg'
+import laptop16 from '../Assets/laptop16.jpg'
+import laptop17 from '../Assets/laptop17.jpg'
+import laptop18 from '../Assets/laptop18.jpg'
+import laptop19 from '../Assets/laptop19.jpg'
+import laptop20 from '../Assets/laptop20.jpg'
+
 
 
 export const productsData = [
   {
-      "image":camera1,
-      "ProductName":"Canon",
+      "image":laptop1,
+      "ProductName":"",
       "Price":"24000",
 
   },
   {
-      "image":camera2,
-      "ProductName":"Sony",
+      "image":laptop2,
+      "ProductName":"",
       "Price":"27000",
 
   },
   {
-      "image":camera3,
-      "ProductName":"Nikon",
+      "image":laptop3,
+      "ProductName":"",
       "Price":"35000",
 
   },
   {
-      "image":camera4,
-      "ProductName":"Sony alpha",
+      "image":laptop4,
+      "ProductName":"",
       "Price":"42000",
 
   }, 
   {
-      "image":camera5,
-      "ProductName":"panasonic",
+      "image":laptop5,
+      "ProductName":"",
       "Price":"28000",
 
   },
   {
-      "image":camera6,
-      "ProductName":"leica",
+      "image":laptop6,
+      "ProductName":"",
       "Price":"16000",
 
   }, 
+  {
+    "image":laptop7,
+    "ProductName":"",
+    "Price":"16000",
 
- 
+}, 
+{
+    "image":laptop8,
+    "ProductName":"",
+    "Price":"16000",
+
+},
+{
+    "image":laptop9,
+    "ProductName":"",
+    "Price":"16000",
+
+},
+{
+    "image":laptop10,
+    "ProductName":"",
+    "Price":"16000",
+
+},
+{
+    "image":laptop11,
+    "ProductName":"",
+    "Price":"16000",
+
+},
+{
+    "image":laptop12,
+    "ProductName":"",
+    "Price":"16000",
+
+},
+{
+    "image":laptop13,
+    "ProductName":"",
+    "Price":"16000",
+
+},
+{
+    "image":laptop14,
+    "ProductName":"",
+    "Price":"16000",
+
+},
+{
+    "image":laptop15,
+    "ProductName":"",
+    "Price":"16000",
+
+},
+{
+    "image":laptop16,
+    "ProductName":"",
+    "Price":"16000",
+
+},
+{
+    "image":laptop17,
+    "ProductName":"",
+    "Price":"16000",
+
+},
+{
+    "image":laptop18,
+    "ProductName":"",
+    "Price":"16000",
+
+},
+{
+    "image":laptop19,
+    "ProductName":"",
+    "Price":"16000",
+
+},
+{
+    "image":laptop20,
+    "ProductName":"",
+    "Price":"16000",
+
+},
+
 ]
